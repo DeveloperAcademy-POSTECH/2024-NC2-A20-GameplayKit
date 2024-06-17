69053440eda30b9f6b5052ed69e2dc6153ee0237
임소연 <ax911@naver.com>
2024-06-17 17:08:29 +0900

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by 임소연 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
